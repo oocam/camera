32129c15e6223c2994c51d00d8a461f5d6443de6
--replace-all <utkarsh867@gmail.com>
2021-01-22 19:06:41 +0800

    Fix bash issues

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sh /home/pi/switch_ap.sh
+bash /home/pi/switch_ap.sh
 git reset --hard
 git pull
-sh /home/pi/switch_ap.sh
+bash /home/pi/switch_ap.sh
 sudo mv /etc/wpa_supplicant/wpa_supplicant.conf.backup /etc/wpa_supplicant/wpa_supplicant.conf
 killall python3
-sh /home/pi/run.sh
+bash /home/pi/run.sh
