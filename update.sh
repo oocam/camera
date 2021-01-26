0f89f5cebeb7ce443fb5573fde10cecce2a58f17
Manya Agarwal <manya18ag@gmail.com>
2021-01-26 16:20:37 +0800

    fix update script paths

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 chmod +x /home/pi/scripts/switch_ap.sh
 chmod +x /home/pi/scripts/update.sh
-bash /home/pi/switch_ap.sh
+bash /home/pi/scripts/switch_ap.sh
 /home/pi/scripts/update.sh
-bash /home/pi/switch_ap.sh
+bash /home/pi/scripts/switch_ap.sh
 reboot
