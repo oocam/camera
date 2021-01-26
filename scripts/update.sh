398473fb2876ccc118347fc0077f8cbe4ad18860
--replace-all <utkarsh867@gmail.com>
2021-01-26 15:26:03 +0800

    Add scripts folder for updates

---
+++ scripts/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git reset --hard
+git pull
+sudo mv /etc/wpa_supplicant/wpa_supplicant.conf.backup /etc/wpa_supplicant/wpa_supplicant.conf
--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+chmod +x /home/pi/scripts/switch_ap.sh
+chmod +x /home/pi/scripts/update.sh
 bash /home/pi/switch_ap.sh
-git reset --hard
-git pull
+/home/pi/scripts/update.sh
 bash /home/pi/switch_ap.sh
-sudo mv /etc/wpa_supplicant/wpa_supplicant.conf.backup /etc/wpa_supplicant/wpa_supplicant.conf
-killall python3
-bash /home/pi/run.sh
+reboot
