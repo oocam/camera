76bfbbecd5b8072f32105487684bf0016e359388
--replace-all <utkarsh867@gmail.com>
2021-02-22 15:21:21 +0800

    Fix wiper module imports

--- openoceancamera/wiper/wiper.py
+++ openoceancamera/wiper/wiper.py
@@ -15,3 +15,14 @@ class Wiper:
     def set_angle(self, angle):
         pwm_output = (SERVO_UPPER_LIMIT * angle + SERVO_LOWER_LIMIT * (360-angle))/360
         os.system(f"gpio -g pwm {self.PIN} {angle}")
+
+def run_wiper(sweeps):
+    wiper_front = Wiper(10)
+    wiper_front.set_angle(0)
+    for i in range(sweeps):
+        wiper_front.set_angle(50)
+        time.sleep(5)
+        wiper_front.set_angle(0)
+
+if __name__ == "__main__":
+    run_wiper(1)
