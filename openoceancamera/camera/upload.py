b57152db8c9f7cd42f2597799ac013c267b0d4a9
--replace-all <utkarsh867@gmail.com>
2021-03-03 19:06:43 +0800

    Add optimisation for upload, add wiper code with correct angle

--- openoceancamera/camera/upload.py
+++ openoceancamera/camera/upload.py
@@ -1,4 +1,5 @@
 import os
+import json
 from datetime import datetime, timedelta
 from constants import EXTERNAL_DRIVE
 from uploader import DropboxUploader
@@ -8,11 +9,20 @@ logging.basicConfig(filename="system_logs.txt", format="%(asctime)s - %(levelnam
 logger = logging.getLogger('main')
 logger.setLevel(logging.DEBUG)
 
+
+if not os.path.exists(os.path.join(EXTERNAL_DRIVE, "uploads.txt")):
+    with open(os.path.join(EXTERNAL_DRIVE, "uploads.txt"), 'w') as uploads:
+        pass
+
 def start_upload(slot):
     logger.info("Starting upload slot")
     upload_handler = DropboxUploader()
+    uploaded_files = []
+    with open(os.path.join(EXTERNAL_DRIVE, "uploads.txt"), 'r') as uploads:
+        for filename in uploads:
+            uploaded_files.append(filename)
     for root, dirs, files in os.walk(EXTERNAL_DRIVE):
-        for f in sorted(files, reverse=True):
+        for f in filter(lambda x: uploaded_files.index(x), sorted(files, reverse=True)):
             if slot["stop"] > datetime.now():
                 logger.info(f"Uploading {f}")
                 upload_handler.upload_file(os.path.join(root, f))
--- openoceancamera/wiper/wiper.py
+++ openoceancamera/wiper/wiper.py
@@ -13,16 +13,16 @@ class Wiper:
         subprocess.run("gpio pwmr 2000",shell=True)
 
     def set_angle(self, angle):
-        pwm_output = (SERVO_UPPER_LIMIT * angle + SERVO_LOWER_LIMIT * (360-angle))/360
+        pwm_output = (SERVO_UPPER_LIMIT * angle + SERVO_LOWER_LIMIT * (270-angle))/270
         subprocess.run(f"gpio -g pwm 18 {pwm_output}", shell=True)
 
 def run_wiper(sweeps):
     wiper_front = Wiper()
-    wiper_front.set_angle(0)
+    wiper_front.set_angle(26)
     for i in range(sweeps):
-        wiper_front.set_angle(70)
+        wiper_front.set_angle(68)
         time.sleep(1)
-        wiper_front.set_angle(0)
+        wiper_front.set_angle(26)
         time.sleep(2)
 
 if __name__ == "__main__":
