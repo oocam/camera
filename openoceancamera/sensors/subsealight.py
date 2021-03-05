1adbc6a59415bbf08f36df0c5ded7f44830710d2
Manya Agarwal <manya18ag@gmail.com>
2021-03-05 13:27:10 +0800

    Integrate subsea light sensor

---
+++ openoceancamera/sensors/subsealight.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import datetime
+import sys
+from logger import logger
+
+class SubSeaLight(Object): 
+
+    def __init__(self):
+        GPIO.setwarnings(False)
+        # originally 11
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.OUT)
+        try: 
+            self.pwm = GPIO.PWM(24,500)  # PIN 12 = Board 32
+        except Exception as err: 
+            logger.error(f"PWM not found")
+    
+    def switch_off(self):
+        self.pwm.ChangeDutyCycle(0)
+        self.pwm.stop()
+
+
+    def switch_on(self,dc):
+        print(dc)
+        self.pwm.start(dc)
+
+
