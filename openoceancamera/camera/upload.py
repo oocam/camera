32aa2929123aedf579f4b70961d3a0d5110411d9
--replace-all <utkarsh867@gmail.com>
2021-03-23 16:51:39 +0800

    Add error handling for no USB

--- openoceancamera/camera/upload.py
+++ openoceancamera/camera/upload.py
@@ -9,23 +9,28 @@ logging.basicConfig(filename="system_logs.txt", format="%(asctime)s - %(levelnam
 logger = logging.getLogger('main')
 logger.setLevel(logging.DEBUG)
 
-
-if not os.path.exists(os.path.join(EXTERNAL_DRIVE, "uploads.txt")):
-    with open(os.path.join(EXTERNAL_DRIVE, "uploads.txt"), 'w') as uploads:
-        pass
+try:
+    if not os.path.exists(os.path.join(EXTERNAL_DRIVE, "uploads.txt")):
+        with open(os.path.join(EXTERNAL_DRIVE, "uploads.txt"), 'w') as uploads:
+            pass
+except:
+    logger.error("There is no USB connected")
 
 def start_upload(slot):
     logger.info("Starting upload slot")
     upload_handler = DropboxUploader()
     uploaded_files = []
-    with open(os.path.join(EXTERNAL_DRIVE, "uploads.txt"), 'r') as uploads:
-        for filename in uploads:
-            uploaded_files.append(filename)
-    for root, dirs, files in os.walk(EXTERNAL_DRIVE):
-        for f in filter(lambda x: uploaded_files.index(x), sorted(files, reverse=True)):
-            if slot["stop"] > datetime.now():
-                logger.info(f"Uploading {f}")
-                upload_handler.upload_file(os.path.join(root, f))
-                logger.info(f"Uploaded {f}")
-            else:
-                break
+    try:
+        with open(os.path.join(EXTERNAL_DRIVE, "uploads.txt"), 'r') as uploads:
+            for filename in uploads:
+                uploaded_files.append(filename)
+        for root, dirs, files in os.walk(EXTERNAL_DRIVE):
+            for f in filter(lambda x: uploaded_files.index(x), sorted(files, reverse=True)):
+                if slot["stop"] > datetime.now():
+                    logger.info(f"Uploading {f}")
+                    upload_handler.upload_file(os.path.join(root, f))
+                    logger.info(f"Uploaded {f}")
+                else:
+                    break
+    except:
+        logger.error("USB Not connected. Nothing to upload")
