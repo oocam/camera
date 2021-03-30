3b44bf46715e784dcdefe097a73a5b8227b63e3a
--replace-all <utkarsh867@gmail.com>
2021-03-30 15:40:43 +0800

    Add environment loader for product key

--- openoceancamera/main.py
+++ openoceancamera/main.py
@@ -11,13 +11,24 @@ from logger import logger
 from camera import camera_thread
 from sensors import Sensor
 from Scheduler import Scheduler
-
+import uuid
 from appserver import start_api_server
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 if __name__ == "__main__":
+    # Generates a product ID first time
+    if not (os.path.exists(".oocamkey") and os.path.isfile(".oocamkey")):
+        with open(".oocamkey", "w") as idfile:
+            generated_id = str(uuid.uuid4())
+            idfile.write(generated_id)
+    
+    # Load environment variables
+    with open(".oocamkey", "r") as idfile:
+        os.environ['CAMERA_UID'] = str(idfile.read()).strip()
+
+    # Start code
     api_thread = threading.Thread(target=start_api_server)
     main_thread = threading.Thread(target=camera_thread)
     api_thread.start()
