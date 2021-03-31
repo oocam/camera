727dd1f9a58c11eb71bbbc4e0b0b5ed2bf628ec0
--replace-all <utkarsh867@gmail.com>
2021-03-31 19:06:17 +0800

    Add progress callback

--- openoceancamera/uploader/s3_uploader.py
+++ openoceancamera/uploader/s3_uploader.py
@@ -5,6 +5,25 @@ import logging
 logging.basicConfig(filename="system_logs.txt", format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+class ProgressPercentage(object):
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
+
 class S3Uploader:
     def __init__(self):
         self.s3 = boto3.client("s3")
@@ -14,7 +33,7 @@ class S3Uploader:
         s3_object_name = CAMERA_UID + "/" + filename.split("/")[-1]
         with open(filename, 'rb') as f:
             try:
-                self.s3.upload_fileobj(f, "oocam-deepsea-store", s3_object_name)
+                self.s3.upload_fileobj(f, "oocam-deepsea-store", s3_object_name, Callback=ProgressPercentage(f))
             except Exception as err:
                 logger.warn(f"Could not upload to S3 bucket, skipping file.\n{err}")
 
@@ -24,10 +43,5 @@ if __name__ == "__main__":
     from datetime import datetime
     upload_handler = S3Uploader()
     ROOT = "/Users/utkarsh/Pictures/test"
-    zipname = os.path.join(ROOT, datetime.now().strftime('%Y-%m-%d_%H-%M-%S')) + ".zip"
-    zipfh = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
-    for root, dirs, files in os.walk(ROOT):
-        for f in filter(lambda x: str(x).endswith(".jpg") or str(x).endswith(".h264"), files):
-            print(os.path.join(root, f))
-            zipfh.write(os.path.join(root, f), os.path.relpath(os.path.join(root, f), os.path.join(ROOT, '..')))
+    zipname = os.path.join(ROOT, "2021-03-31_18-17-34.zip")
     upload_handler.upload_file(zipname)
