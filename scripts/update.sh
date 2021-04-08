5d155a4bc656dcc49a99588d283f0decf22eb43b
--replace-all <utkarsh867@gmail.com>
2021-04-08 15:49:52 +0800

    Add installation in update from requirements

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git reset --hard
 git pull
+pip install -r openoceancamera/requirements.txt
 cp $HOME/etc/dhcpcd.conf /etc/dhcpcd.conf.backup
 sudo mv /etc/wpa_supplicant/wpa_supplicant.conf.backup /etc/wpa_supplicant/wpa_supplicant.conf
