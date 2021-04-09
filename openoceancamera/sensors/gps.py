d79093db1c339b4093494214780ff920cb0f2c01
--replace-all <utkarsh867@gmail.com>
2021-04-09 15:45:25 +0800

    Fix threading lock issues with GPS

--- openoceancamera/sensors/gps.py
+++ openoceancamera/sensors/gps.py
@@ -10,16 +10,18 @@ class GPS(GPS_GtopI2C):
         super().__init__(board.I2C())
         self.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
         self.send_command(b"PMTK220,1000")
-        threading.Timer(1, self.update).start()
-        threading.Timer(5, self.request_firmware).start()
+        threading.Thread(target=self.update).start()
+        threading.Thread(target=self.request_firmware).start()
 
     def request_firmware(self):
         self.send_command(b"PMTK605")
-        threading.Timer(5, self.request_firmware).start()
+        time.sleep(5)
+        self.request_firmware()
 
     def update(self):
         super().update()
-        threading.Timer(1, self.update).start()
+        time.sleep(1)
+        self.update()
 
 
 if __name__ == "__main__":
