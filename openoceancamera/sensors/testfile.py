b2cbdda9f874aed6ee9b40c825a42b4d2b9100a0
Utkarsh Goel <utkarsh867@gmail.com>
2021-07-20 13:21:09 +0800

    Fix import error and incorrect method name

--- openoceancamera/sensors/testfile.py
+++ openoceancamera/sensors/testfile.py
@@ -1,6 +1,6 @@
 """Temporary testfile to sort out list index error in code."""
 
-from .atlas_sensors import EC_Sensor, PH_Sensor, DO_Sensor
+import atlas_sensors
 
 # Initialise the three sensors.
 # NOTE: they should initialise in the following order, so check the order! 
@@ -8,26 +8,26 @@ from .atlas_sensors import EC_Sensor, PH_Sensor, DO_Sensor
 
 print('Initialisation check!')
 print('First the EC sensor')
-ec_sensor = EC_Sensor()
+ec_sensor = atlas_sensors.EC_Sensor()
 
 print('then the DO sensor')
-do_sensor = DO_Sensor()
+do_sensor = atlas_sensors.DO_Sensor()
 
 print('First the PH sensor')
-ph_sensor = PH_Sensor()
+ph_sensor = atlas_sensors.PH_Sensor()
 
 # Now lets see the readings. The faulty part is that some of the _get_data calls 
 # appear to not be returning an entire list of values
 # Hench the 'index error'.
 
 print('EC sensor should return a list of 4 vals')
-print(ec_sensor._get_data())
+print(ec_sensor.get_data())
 
 print('DO sensor should return 2 vals:')
-print(ec_sensor._get_data())
+print(ec_sensor.get_data())
 
 print('PH sensor should return 1 val')
-print(ph_sensor._get_data())
+print(ph_sensor.get_data())
 
 print('Now lets check the specific get data methods')
 print('EC sensor')
